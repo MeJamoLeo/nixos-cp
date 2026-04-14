3d95b0e72e2e99fe3b2221817d377ca5712f9755
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2026-04-14 16:16:11 -0500

    add home modules

---
+++ home/shell.nix
@@ -0,0 +1,19 @@
+{...}: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    initContent = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      bindkey -e  # emacsモードを有効化（Ctrl+N, Ctrl+Pが使えるようになる）
+    '';
+  };
+
+  home.shellAliases = {
+    k = "kubectl";
+
+    urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+    urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+
+    cdsp = "claude --dangerously-skip-permissions";
+  };
+}
---
+++ hosts/x1carbon/home.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }:
+
+{
+	imports = [
+		../../home/shell.nix
+		../../home/git.nix
+		../../home/starship.nix
+	];
+
+	home.username = "treo";
+	home.homeDirectory = "/home/treo";
+
+	home.packages = with pkgs; [
+		git
+			htop
+			wget
+			curl
+	];
+
+	programs.home-manager.enable = true;
+
+	home.stateVersion = "24.11";
+}
