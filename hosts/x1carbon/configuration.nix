605714b9154b65aec40ff4869703af871f779408
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2026-04-14 16:08:57 -0500

    init: configuration.nix

---
+++ hosts/x1carbon/configuration.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "x1carbon";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Chicago";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  users.users.treo = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "input" ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    wget
+    curl
+  ];
+
+  programs.sway.enable = true;
+  programs.zsh.enable = true;
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+  ];
+
+  system.stateVersion = "24.11";
+}
