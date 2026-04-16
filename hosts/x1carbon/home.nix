56b6c9b581b09b9c4eb013790af20f2a40d4fc85
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2026-04-16 15:03:02 -0500

    fix: revert to enabled="fcitx5" (home-manager version doesn't support enable/type)

--- hosts/x1carbon/home.nix
+++ hosts/x1carbon/home.nix
@@ -41,15 +41,11 @@
 
 	# Japanese input (fcitx5 + mozc)
 	i18n.inputMethod = {
-		enable = true;
-		type = "fcitx5";
-		fcitx5 = {
-			waylandFrontend = true;
-			addons = with pkgs; [
-				fcitx5-mozc
-				fcitx5-gtk
-			];
-		};
+		enabled = "fcitx5";
+		fcitx5.addons = with pkgs; [
+			fcitx5-mozc
+			fcitx5-gtk
+		];
 	};
 
 	xdg.configFile."fcitx5/profile" = {
