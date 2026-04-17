1b688bbbec5a2fb8f35f22fb08f1701fa154fd8f
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2026-04-16 23:32:54 -0500

    feat: python3, CJK fonts, fcitx5+mozc declarative (waylandFrontend, settings.inputMethod)

--- hosts/x1carbon/configuration.nix
+++ hosts/x1carbon/configuration.nix
@@ -30,6 +30,7 @@
     wofi
     brightnessctl
     pulseaudio
+    python3
     online-judge-tools
     fzf
   ];
@@ -45,6 +46,42 @@
   # CapsLock/Ctrl swap at TTY level
   console.useXkbConfig = true;
   services.xserver.xkb.options = "ctrl:swapcaps";
+
+  # Japanese input (fcitx5 + mozc)
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+      ];
+      settings = {
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "mozc";
+            Layout = "";
+          };
+          GroupOrder = {
+            "0" = "Default";
+          };
+        };
+      };
+    };
+  };
+
+  # Sway needs XDG autostart for fcitx5
+  services.xserver.desktopManager.runXdgAutostartIfNone = true;
 
   # TLP power management
   services.tlp.enable = true;
@@ -75,6 +112,7 @@
 
   fonts.packages = with pkgs; [
     noto-fonts
+    noto-fonts-cjk-sans
     noto-fonts-emoji
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
